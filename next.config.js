8c6f1088434134bbc208493c52c49ad293123f35
shaakilahammed <shaakilahammed@gmail.com>
2025-01-01 17:28:03 +0600

    setup: initial template

---
+++ next.config.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: process.env.BASEPATH,
+
+  // TODO: below line is added to resolve twice event dispatch in the calendar reducer
+  reactStrictMode: false
+}
+
+module.exports = nextConfig
